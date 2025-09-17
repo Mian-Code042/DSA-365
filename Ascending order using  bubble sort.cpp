24e7258ce92e93130205123dcf6ed8be6eb0e43c
Mian Nisar ul Hassan <miannisarulhassan0@gmail.com>
2025-09-17 21:39:08 +0545

    Dsa-Streak

---
+++ Ascending order using  bubble sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+void bubble(int arr[],int n) {
+    for (int i=0;i<n-1;i++) {
+        bool swapped=false;
+        for (int j=0;j<n-i-1;j++) {
+            if (arr[j]>arr[j+1]) {  
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                swapped=true;
+            }
+        }
+        if(!swapped )break;
+    }
+}
+int main() {
+    int n;
+    cout<<"Enter number of elements you want to enter in an array:";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+    	cin>>arr[i];
+	}
+    int no=sizeof(arr)/sizeof(arr[0]);
+    cout<<"Original array:";
+    for(int i= 0;i<n;i++){
+	cout << arr[i]<<" ";
+}
+    cout<<endl;
+    bubble(arr, no);
+    cout<<"Sorted Ascending array using bubble sort:";
+    for (int i=0;i<n;i++) {
+	cout<<arr[i]<<" ";
+}
+}
